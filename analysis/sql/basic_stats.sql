4400b9044386b1c620a07f001ba164194b10f081
heungh <82863575+heungh@users.noreply.github.com>
2025-08-31 17:04:30 +0900

    Delete analysis directory

--- analysis/sql/basic_stats.sql
+++
@@ -1,8 +0,0 @@
-
--- 기본 통계
-SELECT 
-    COUNT(*) as total_requests,
-    COUNT(DISTINCT regexp_extract(client_ip_port, '^([^:]+)', 1)) as unique_ips,
-    AVG(response_time1) as avg_response_time1,
-    AVG(response_time2) as avg_response_time2
-FROM accesslog_analytics.access_logs;
